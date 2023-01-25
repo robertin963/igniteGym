bd3ffbd43bb3809f70bae6da786bad852d9eacdc
Roberto C Pereira <roberto.maranata@gmail.com>
2023-01-25 11:39:46 -0300

    Lista de exercicios

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,58 @@
 import { useState } from 'react';
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base';
 import { GroupHome } from '@components/GroupHome';
 import { HomeHeader } from '@components/HomeHeader';
-import { Center, HStack, Text, VStack } from 'native-base';
+import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home(){
+  const [groups, setGroups] = useState(['Costas','Bíceps','Tríceps', 'ombro']);
   const [groupSelected, setGroupSelected] = useState('ombro');
+  const [exercicios, setExercicios] = useState(['1','2','3','4','5','Puxada frontal','Remada unilateral','Remada curvada', 'Levantamento terras']);
 
   
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <GroupHome name='costa' isActive={groupSelected === 'costa'} onPress={() => setGroupSelected("costa")}/>
-        <GroupHome name='ombro' isActive={groupSelected === 'ombro'} onPress={() => setGroupSelected("ombro")}/>
-        <GroupHome name='perna' isActive={groupSelected === 'perna'} onPress={() => setGroupSelected("perna")}/>
 
-      </HStack>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupHome name={item} isActive={groupSelected === item} onPress={() => setGroupSelected(item)}/>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+        minH={10}
+      />       
+
+      <VStack px={8}>
+        <HStack mb={5}>
+          <Heading color="gray.200" fontSize="md" flex={1} >
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercicios.length}
+          </Text>
+        </HStack>
+
+
+        <FlatList 
+          data={exercicios}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20}}
+          
+        />
+
+      </VStack>
+      
     </VStack>
 
   );
